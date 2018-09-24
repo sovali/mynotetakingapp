f2bc4bedc557d66a268ad44fa4d7efded0b08cb5
Selin Ovali <selinovali@pal-nat186-142-248.itap.purdue.edu>
2018-09-24 13:30:05 -0400

    Add html to create different sections & create function to add Notes

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
+
+class Note {
+    constructor (id, name, description) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+    }
+}
 class NoteComponent {
     constructor (noteList, query) {
-        this.noteList = noteList;
+        this.noteList = [noteList];
         this.query = query;
     }
 
     filterList(noteList, query) {
-    
-        noteList.forEach(function(Note) {
-            if (Note.name === query) {
-
-            }
+        let results = this.noteList.filter(note => {
+            return (note.name.indexOf(query) != -1);
         })
     }
 }
+let noteList = [];
+let addBtn = document.getElementById('addBtn');
+let noteCount = 0;
 
-class Note {
-    constructor (name, description) {
-        this.name = name;
-        this.description = description;
+addBtn.addEventListener('click', function(e) {
+    let name = document.getElementById('title').value;
+    let content = document.getElementById('content').value;
+    if (name.length !== 0 && content.length !== 0) {
+        let note = new Note(noteCount, name, content);
+        noteCount++;
+        addNote(note);
+        clear('title');
+        clear('content');
+        renderList();
+    } else {
+        alert("Please fill in title and content to add.")
+    }
+    return;
+})
+
+function renderList() {
+    if (noteList.length!=0) {
+        for (let i = 0; i < noteList.length; i++) {
+            let name = noteList[i].name;
+            let content = noteList[i].description;
+            console.log(name + " " + content);
+        }
     }
 }
+
+// Clears value of element
+function clear(id) {
+    document.getElementById(id).value="";
+}
+// Adds note to noteList array & the page
+function addNote(note) {
+   noteList.push(note);
+
+   for (let i = 0; i < noteList.length; i++) {
+
+   }
+}
+
+
